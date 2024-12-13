597f52c6b5350a71908b53874c6c3b35c012f9d8
Slavcho Mitrov <mitrov.slavcho@gmail.com>
2024-12-13 15:40:08 +0100

    create modal

---
+++ components/Modal.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div v-if="isOpen" class="fixed inset-0 z-50 bg-black bg-opacity-60"></div>
+
+    <div
+      v-if="isOpen"
+      class="fixed inset-0 z-50 flex items-center justify-center"
+    >
+      <div
+        class="relative max-h-full max-w-full rounded-3xl bg-white p-6 shadow-lg"
+        :style="{
+          width: '90vw',
+          height: '90vh',
+          maxWidth: '1200px',
+          maxHeight: '90vh',
+        }"
+      >
+        <button
+          class="absolute right-4 top-4 rounded bg-black p-2 text-white"
+          @click="toggleModal"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+        <div class="overflow-auto">
+          <iframe
+            :src="projectUrl"
+            class="h-[80vh] w-full"
+            frameborder="0"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  projectUrl: String,
+});
+
+const isOpen = useState("isOpen", () => false);
+
+function toggleModal() {
+  isOpen.value = !isOpen.value;
+}
+</script>
--- components/Projects.vue
+++ components/Projects.vue
@@ -4,42 +4,55 @@
       <h2 class="mb-20 text-[104px] font-extrabold text-[#E9E9E9]">
         Some projects
       </h2>
-      <div>
-        <div class="grid auto-rows-auto sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-10">
-          <div
-            v-for="(project, index) in projects"
-            :key="index"
-            :class="['project', 'flex', 'flex-col', 'justify-between', 'rounded-3xl', 'bg-[#E9E9E9]', 'px-9', 'py-5', project.span === 2 ? 'lg:col-span-2' : '']"
-          >
-            <div>
-              <h3 class="mb-4 text-[32px] text-black">
-                {{ project.name }}
-              </h3>
-              <p class="line-clamp-4 max-w-[80%] text-lg text-[#232323]">
-                {{ project.description }}
-              </p>
-            </div>
-            <button
-              class="mt-4 flex h-[51px] min-w-[81px] items-center justify-center self-end rounded-full bg-black text-white transition-all duration-500 hover:bg-white hover:text-black"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="size-11"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-                />
-              </svg>
-            </button>
+      <div
+        class="grid auto-rows-auto gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3"
+      >
+        <div
+          v-for="(project, index) in projects"
+          :key="index"
+          :class="[
+            'project',
+            'flex',
+            'flex-col',
+            'justify-between',
+            'rounded-3xl',
+            'bg-[#E9E9E9]',
+            'px-9',
+            'py-5',
+            project.span === 2 ? 'lg:col-span-2' : '',
+          ]"
+        >
+          <div>
+            <h3 class="mb-4 text-[32px] text-black">
+              {{ project.name }}
+            </h3>
+            <p class="line-clamp-4 max-w-[80%] text-lg text-[#232323]">
+              {{ project.description }}
+            </p>
           </div>
+          <button
+            @click="openModal"
+            class="mt-4 flex h-[51px] min-w-[81px] items-center justify-center self-end rounded-full bg-black text-white transition-all duration-500 hover:bg-white hover:text-black"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-11"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+              />
+            </svg>
+          </button>
         </div>
       </div>
+
+      <Modal />
     </div>
   </div>
 </template>
@@ -88,4 +101,10 @@ const generateRandomProjects = () => {
 onMounted(() => {
   projects.value = generateRandomProjects();
 });
+
+const modalOpen = useState('isOpen');
+
+function openModal() {
+  modalOpen.value = !modalOpen.value;
+}
 </script>
