4d68505650193064397eabbf2981f5acda9013fb
Slavcho Mitrov <mitrov.slavcho@gmail.com>
2025-12-02 12:54:55 +0100

    disable aos on mobile via utility classes because it's not working properly with mobile properly in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -80,7 +80,6 @@ export default defineNuxtConfig({
     },
   },
   aos: {
-    disable: "mobile",
     once: true, // whether animation should happen only once - while scrolling down
     anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
   },
